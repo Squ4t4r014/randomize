b0f3852aee0043e1efad9194ebd5cb82a6068fc1
Squatarola <b1017150@fun.ac.jp>
2021-10-14 15:25:48 +0900

    --richオプションを追加

--- randomize.py
+++ randomize.py
@@ -1,11 +1,14 @@
-import sys
+import argparse
 import secrets
 
-elems = sys.argv
-n_elems = []
+# 引数のパーサー
+a_parser = argparse.ArgumentParser(description="Randomize inputs.")
+a_parser.add_argument("--rich", action="store_true", help="Provide rich view.")
+a_parser.add_argument("elems", nargs="*", help="Some elements that is need randomized.")
 
-# argv[0]はスクリプトのパスなので削除
-del elems[0]
+args = a_parser.parse_args()
+elems = args.elems
+n_elems = []
 
 i = len(elems)
 
@@ -21,4 +24,34 @@ while i > 0:
     del elems[p_index]
     i-=1
 
-print(n_elems)
+if args.rich:
+    for i in range(len(n_elems)):
+        e = n_elems[i]
+
+        # 選出順位を表示
+        print(i, end = "")
+
+        # 上罫線の表示
+        j = 0
+        while j < len(e) + 3:
+            print("-", end = "")
+            j += 1
+
+        # 選出されたelementを表示
+        print("\n|", e, "|")
+
+        # 下罫線の表示
+        j = 0
+        while j < len(e) + 4:
+            print("-", end = "")
+            j += 1
+
+        # リスト最後の"↓"は不要なのでbreak
+        if i == len(n_elems) - 1:
+            print()
+            break
+
+        print("\n↓")
+
+else:
+    print(n_elems)
